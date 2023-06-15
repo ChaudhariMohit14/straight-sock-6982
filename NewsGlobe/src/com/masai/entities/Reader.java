0b713b3c8f267bdd51f6f428ed08fee603739099
unknown <chaudharimohit1410@gmail.com>
2023-06-15 23:57:16 +0530

    Pushing day-2 task

---
+++ NewsGlobe/src/com/masai/entities/Reader.java
@@ -0,0 +1,49 @@
+package com.masai.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reader extends User implements Serializable{
+	private static List<String> registeredUsernames = new ArrayList<>();
+	
+	public Reader(String username, String password, String name, String email) {
+        super(username, password, name, email);
+    }
+	
+	public static Reader signUp(String username, String password, String name, String email) {
+        // Check if the username already exists
+        if (checkUsernameExists(username)) {
+            System.out.println("Username already exists. Please choose a different username.");
+            return null;
+        }
+
+        // Create a new Reader object
+        Reader reader = new Reader(username, password, name, email);
+        registeredUsernames.add(username); 
+        System.out.println("Reader signed up successfully.");
+
+        return reader;
+    }
+	
+	private static boolean checkUsernameExists(String username) {
+        // Check if the username exists in the list
+        return registeredUsernames.contains(username);
+    }
+	
+	
+	
+	public boolean login(String username, String password) {
+        if (getUsername().equals(username) && getPassword().equals(password)) {
+            System.out.println("Reader logged in.");
+            return true;
+        } else {
+            System.out.println("Invalid username or password.");
+            return false;
+        }
+    }
+
+    public void logout() {
+        System.out.println("Reader logged out.");
+    }
+}
